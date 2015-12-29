707b2f78910511bb6e07f0d97d915bbb578222b7
Cody Daniels <codydaniels@gmail.com>
2015-12-29 11:42:10 -0700

    Allow user to edit polls

--- client/app/poll/poll.controller.js
+++ client/app/poll/poll.controller.js
@@ -7,6 +7,7 @@ class PollController {
   constructor($http, $scope, $routeParams, Auth, socket) {
     var app = this;
     $scope.isVoting = true;
+    app.isEditing = false;
 
     $scope.labels = [];
     $scope.data = [[]];
@@ -17,29 +18,80 @@ class PollController {
       socket.syncUpdates('poll', [app.poll]);
     });
 
-    app.vote = function() {
-      $scope.isVoting = false;
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('poll');
+    });
+
+    app.isOwner = function() {
+      if (Auth.getCurrentUser().email === app.poll.createdBy) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    app.toggleEdit = function() {
+      app.isEditing = true;
+    };
+
+    app.deleteOption = function(option) {
       var optionsArray = [];
-      app.poll.options.forEach(function(option) {
-        if (option.answer === $scope.choice) {
-          optionsArray.push({
-            answer: option.answer,
-            count: (option.count + 1)
-          });
-          option.count++;
-        } else {
-          optionsArray.push({
-            answer: option.answer,
-            count: option.count
-          });
+      app.poll.options.forEach(function(pollOption) {
+        if (pollOption.answer !== option.answer) {
+          optionsArray.push(pollOption);
         }
       });
-      $http.put('/api/polls/' + $routeParams.id, optionsArray).success(function() {
-        app.poll.options.forEach(function(option) {
-          $scope.labels.push(option.answer);
-          $scope.data[0].push(option.count);
+      $http.put('/api/polls/' + $routeParams.id, optionsArray);
+      app.poll.options.splice(app.poll.options.indexOf(option), 1);
+    };
+
+    app.addOption = function(newOption) {
+      var optionsArray = [];
+      app.poll.options.forEach(function(pollOption) {
+        optionsArray.push({
+          answer: pollOption.answer,
+          count: pollOption.count
         });
       });
+      optionsArray.push({
+        answer: newOption,
+        count: 0
+      });
+      $http.put('/api/polls/' + $routeParams.id, optionsArray);
+      app.poll.options.push({
+        answer: newOption,
+        count: 0
+      });
+      $scope.newOption = '';
+    };
+
+    app.vote = function() {
+      if ($scope.choice === undefined) {
+
+      } else {
+        $scope.isVoting = false;
+        var optionsArray = [];
+        app.poll.options.forEach(function(option) {
+          if (option.answer === $scope.choice) {
+            optionsArray.push({
+              answer: option.answer,
+              count: (option.count + 1)
+            });
+            option.count++;
+          } else {
+            optionsArray.push({
+              answer: option.answer,
+              count: option.count
+            });
+          }
+        });
+        $http.put('/api/polls/' + $routeParams.id, optionsArray).success(function() {
+          app.poll.options.forEach(function(option) {
+            $scope.labels.push(option.answer);
+            $scope.data[0].push(option.count);
+          });
+        });
+      }
     };
   }
 }
